82b24656de0cfebf07343edc3f4e22d4c7698793
eric-nichols-nyc <ebn646@gmail.com>
2025-05-09 10:29:45 -0400

    feat: sticky header for projects

--- src/routes/projects/index.tsx
+++ src/routes/projects/index.tsx
@@ -9,8 +9,10 @@ export const Route = createFileRoute('/projects/')({
 function ProjectsIndex() {
   return (
     <ContentContainer>
+      <div className="sticky top-0 z-10 bg-[#19191b] border-b border-[#232225] py-4 mb-6">
+        <h1 className="text-3xl font-bold text-white">Projects</h1>
+      </div>
       <div>
-        <h1>Projects</h1>
         <ul>
         {projects.map((project) => (
           <li key={project.id}>
