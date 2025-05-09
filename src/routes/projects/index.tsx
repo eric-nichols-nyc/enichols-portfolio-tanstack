a6e45651641bc8388a430de9863b9efe8563e857
eric-nichols-nyc <ebn646@gmail.com>
2025-05-09 10:27:04 -0400

    feat: add linting

--- src/routes/projects/index.tsx
+++ src/routes/projects/index.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute, Link } from '@tanstack/react-router'
 import projects from '@/data/projects'
+import ContentContainer from '@/components/content-container'
 
 export const Route = createFileRoute('/projects/')({
   component: ProjectsIndex,
@@ -7,9 +8,10 @@ export const Route = createFileRoute('/projects/')({
 
 function ProjectsIndex() {
   return (
-    <div>
-      <h1>Projects</h1>
-      <ul>
+    <ContentContainer>
+      <div>
+        <h1>Projects</h1>
+        <ul>
         {projects.map((project) => (
           <li key={project.id}>
             <Link to="/projects/$projectId" params={{ projectId: project.id }}>
@@ -19,5 +21,6 @@ function ProjectsIndex() {
         ))}
       </ul>
     </div>
+    </ContentContainer>
   )
 }
